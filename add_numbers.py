bddce394ce8c1898e83700d6baaa253cce549008
Kalpak <kalpakto26@gmail.com>
2022-04-19 13:24:26 +0530

    Added test case for blank input

--- add_numbers.py
+++ add_numbers.py
@@ -1,3 +1,12 @@
 """
 The main module
 """
+
+
+def add_numbers(numbers: str) -> int:
+    """
+    Take upto two numbers in string format and return their sum in integer format 
+    """
+    # Input can be an empty string too
+    if numbers == '':
+        return 0
--- test_add_numbers.py
+++ test_add_numbers.py
@@ -1,3 +1,20 @@
 """
 Test module for add_numbers.py
 """
+
+
+import unittest2
+from add_numbers import add_numbers
+
+
+class TestAddNumbers(unittest2.TestCase):
+    """
+    Test the function add_numbers from module add_numbers.py
+    """
+
+    def test_empty_string(self):
+        self.assertEqual(add_numbers(''), 0)
+
+
+if __name__ == '__main__':
+    unittest2.main()
