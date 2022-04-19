e8f67afc766490b82148f004ea2c0185ae01af9a
Kalpak <kalpakto26@gmail.com>
2022-04-19 13:30:39 +0530

    Added test case for single integer inputs

--- add_numbers.py
+++ add_numbers.py
@@ -4,9 +4,12 @@ The main module
 
 
 def add_numbers(numbers: str) -> int:
-    """
-    Take upto two numbers in string format and return their sum in integer format 
-    """
-    # Input can be an empty string too
-    if numbers == '':
-        return 0
+	"""
+	Take upto two numbers in string format and return their sum in integer format 
+	"""
+	# Input can be an empty string too
+	if numbers == '':
+		return 0
+	
+	if len(numbers) == 1:
+		return int(numbers)
--- test_add_numbers.py
+++ test_add_numbers.py
@@ -8,13 +8,17 @@ from add_numbers import add_numbers
 
 
 class TestAddNumbers(unittest2.TestCase):
-    """
-    Test the function add_numbers from module add_numbers.py
-    """
+	"""
+	Test the function add_numbers from module add_numbers.py
+	"""
 
-    def test_empty_string(self):
-        self.assertEqual(add_numbers(''), 0)
+	def test_empty_string(self):
+		self.assertEqual(add_numbers(''), 0)
+
+	def test_single_integer(self):
+		self.assertEqual(add_numbers('1'), 1)
+		self.assertEqual(add_numbers('2'), 2)
 
 
 if __name__ == '__main__':
-    unittest2.main()
+	unittest2.main()
