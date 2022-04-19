dbb544e3fdfaf59eaa40d8a3f61e0f2a27285869
Kalpak <kalpakto26@gmail.com>
2022-04-19 18:46:40 +0530

    Handled negative numbers

--- add_numbers.py
+++ add_numbers.py
@@ -7,18 +7,36 @@ class InvalidInputException(Exception):
 	def __str__(self):
 		return 'Invalid input.'
 
+
+class NegativeNumberException(Exception):
+	def __str__(self):
+		return 'Negatives not allowed'
+
+
 def add_numbers(numbers: str) -> int:
 	"""
-	Take upto two numbers in string format and return their sum in integer format 
+	Take upto two numbers in string format and return their sum in integer format
 	"""
 	# Input can be an empty string too
 	if numbers == '':
 		return 0
-	
-	if len(numbers) >= 1:
-		numbers = numbers.replace('\n',',')
-		try:
-			nums = list(map(int, numbers.split(',')))
-			return sum(nums)
-		except:
-			raise InvalidInputException
+
+	if numbers[:2] == '//':
+		delimiter = numbers[2]
+		numbers = numbers[3:].replace('\n', '')
+	else:
+		delimiter = ','
+		numbers = numbers.replace('\n', ',')
+	nums = numbers.split(delimiter)
+	if '' in nums:
+		raise InvalidInputException
+	else:
+		negs = []
+		for num in nums:
+			if num[0] == '-':
+				negs.append(num)
+		if len(negs) >= 1:
+			print('Negative inputs:',*negs)
+			raise NegativeNumberException
+	return sum(list(map(int, nums)))
+
--- test_add_numbers.py
+++ test_add_numbers.py
@@ -4,7 +4,7 @@ Test module for add_numbers.py
 
 
 import unittest2
-from add_numbers import InvalidInputException, add_numbers
+from add_numbers import InvalidInputException, NegativeNumberException, add_numbers
 
 
 class TestAddNumbers(unittest2.TestCase):
@@ -30,7 +30,17 @@ class TestAddNumbers(unittest2.TestCase):
 		with self.assertRaises(InvalidInputException):
 			add_numbers('1,\n\n,,')
 
-
+	def test_different_delimiter(self):
+		self.assertEqual(add_numbers('//;\n1;2'), 3)
+		self.assertEqual(add_numbers('//:\n10:20:55'), 85)
+	
+	def test_negative_numbers(self):
+		with self.assertRaises(NegativeNumberException):
+			add_numbers('1,-2')
+		with self.assertRaises(NegativeNumberException):
+			add_numbers('1\n-2,3')
+		with self.assertRaises(NegativeNumberException):
+			add_numbers('//;\n1;-2')
 
 if __name__ == '__main__':
 	unittest2.main()
